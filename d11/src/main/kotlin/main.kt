a60125be7ef55880d4b3e8c80666872f1d2b47e1
berhalak <jaroslaw.sadzinski@gmail.com>
2020-12-11 11:42:21 +0100

    d11

--- d11/src/main/kotlin/main.kt
+++ d11/src/main/kotlin/main.kt
@@ -1,164 +1,236 @@
 import java.io.File
 
+val EMPTY = 1
+val FLOOR = 0
+val TAKEN = 2
 
-interface Device : Comparable<Device>, Iterable<Device> {
-    fun can(prev: Device) : Boolean {
-        return false;
+open class Seat(val row: Int, val column: Int, val room: Room) {
+
+    open fun adjecntLimit() = 4
+
+    fun isEmpty(): Boolean {
+        return room.at(row, column) == EMPTY;
     }
 
-    fun output(): Long {
-        return 0
+    fun shift(other: Room) {
+        val seat = this;
+        val copy = other
+
+        if (seat.isEmpty() && seat.adjectCount() == 0) {
+            seat.makeOcupy(copy)
+        } else if (seat.isOccupied() && seat.adjectCount() >= adjecntLimit()) {
+            seat.makeEmpty(copy)
+        }
     }
 
-    fun addPossible(def: List<Device>);
+    open fun adjectCount(): Int {
+        fun at(row: Int, column: Int) : Int {
+            if (row < 0) return 0
+            if (column < 0) return 0
+            if (row > room.rows.lastIndex) return 0
+            if (column > room.rows[0].lastIndex) return 0
+            return if (room.at(row,column) == TAKEN) 1 else 0;
+        }
 
-    fun total() : Long {
-        return 0
+        val count = at(row - 1, column - 1) + at(row - 1, column) + at(row - 1, column + 1) +
+                    at(row, column - 1) + at(row, column + 1) +
+                    at(row + 1, column - 1) + at(row + 1, column) + at(row + 1, column + 1)
+
+        return count
     }
 
-    fun setInput(i: Device) {
-
+    fun makeOcupy(copy: Room) {
+        copy.rows[row][column] = TAKEN;
     }
 
-    fun getInput() : Device? {
-        return null;
+    fun isOccupied() : Boolean {
+        return room.rows[row][column] == TAKEN
     }
 
-    fun diffrence() : Long {
-        if (getInput() == null) return 0;
-        return output() - getInput()!!.output()
+    fun makeEmpty(copy: Room) {
+        copy.rows[row][column] = EMPTY;
     }
-
-    override fun compareTo(other: Device): Int {
-        return this.output().compareTo(other.output())
-    }
-
-    override fun iterator(): Iterator<Device> {
-        return sequence<Device> {  }.iterator()
-    }
-
 
 }
 
-
-
-class Adapter(val rating: Long) : Device {
-    override fun output(): Long {
-        return rating;
+class Seat2(row: Int, column: Int, room: Room) : Seat(row, column, room) {
+    override fun adjecntLimit(): Int {
+        return 5
     }
 
-    override fun toString(): String {
-        return output().toString()
+    override fun adjectCount(): Int {
+        fun point(row: Int, column: Int) : Int {
+            if (row < 0) return 0
+            if (column < 0) return 0
+            if (row > room.rows.lastIndex) return 0
+            if (column > room.rows[0].lastIndex) return 0
+            return if (room.at(row,column) == TAKEN) 1 else 0;
+        }
+
+        fun at(dRow: Int, dColumn: Int) : Int {
+            var startRow = row;
+            var startColumn = column;
+
+            while(true) {
+                startRow = startRow + dRow
+                startColumn = startColumn + dColumn
+
+                if (startRow < 0) break;
+                if (startColumn < 0) break;
+                if (startRow > room.rows.lastIndex) break;
+                if (startColumn > room.rows[0].lastIndex) break;
+                if (room.at(startRow,startColumn) == TAKEN) return 1;
+                if (room.at(startRow, startColumn) != FLOOR) return 0;
+            }
+             return 0
+        }
+
+
+        val count = at( - 1,  - 1) + at( - 1, 0) + at( - 1,  1) +
+                at(0, 0 - 1) + at(0, 0 + 1) +
+                at(0 + 1, 0 - 1) + at(0 + 1, 0) + at(0 + 1, 0 + 1)
+
+        return count
+    }
+}
+
+class Room : Iterable<Seat> {
+    val rows : Array<IntArray>;
+
+    var gen : (Int, Int, Room) -> Seat = { x, y, r -> Seat(x, y, r)}
+
+    constructor(input: List<String>, generator: (Int, Int, Room) -> Seat) {
+        val rows = input.size;
+        gen = generator
+
+        val columns = input[0].length
+        this.rows = Array<IntArray>(rows) { IntArray(columns) { 0 } }
+
+        for(index in input.indices) {
+            val line = input[index]
+            for(charIndex in line.indices) {
+                val ch = line[charIndex]
+                this.rows[index][charIndex] = when (ch) {
+                    'L' -> EMPTY
+                    '.' -> FLOOR
+                    else -> TAKEN
+                }
+            }
+        }
     }
 
-    var children: List<Device> = listOf()
+    constructor(copy: Room, generator: (Int, Int, Room) -> Seat) : this(copy.rows, generator) {
 
-    override fun addPossible(def: List<Device>) {
-        children = def
     }
 
-    var _total = -1L
-
-    override fun total(): Long {
-        if (children.size == 0) return 1;
-
-        if (_total != -1L) return _total
-
-        _total = children.map { it.total() }.sum()
-
-        return _total
+    constructor(other: Array<IntArray>, generator: (Int, Int, Room) -> Seat) {
+        gen = generator
+        rows = Array(other.size) { IntArray(other[0].size) { 0 } }
+        for(index in other.indices) {
+            val line = other[index]
+            for(charIndex in line.indices) {
+                rows[index][charIndex] = line[charIndex]
+            }
+        }
     }
 
-    private var _input: Device? = null;
+    fun print() {
 
-    override fun setInput(i: Device) {
-        _input = i;
+        println("------------------")
+
+        for(rowIndex in rows.indices) {
+            val row = rows[rowIndex]
+            for(colIndex in row.indices) {
+                val col = row[colIndex]
+
+                print(when(col) {
+                    TAKEN -> '#'
+                    FLOOR -> '.'
+                    else -> 'L'
+                })
+            }
+            println()
+        }
+
+        println("------------------")
     }
 
-    override fun getInput() : Device? {
-        return _input;
+    fun ocupy() : Room {
+        val copy = Room(this, gen)
+        for(seat in this) {
+            seat.shift(copy);
+        }
+        return copy
     }
 
-    override fun iterator(): Iterator<Device> {
-        var start : Device? = this as Device
-        val s = sequence<Device> {
-            while(start != null) {
-                yield(start as Device)
-                start = start!!.getInput()
+    fun ocupied() : Int {
+        val count = this.filter { it.isOccupied() }.count()
+        return count;
+    }
+
+    fun at(x: Int, y: Int) = rows[x][y];
+
+    override fun iterator(): Iterator<Seat> {
+        val room = this
+        val s = sequence<Seat> {
+            for(index in rows.indices) {
+                val line = rows[index]
+                for(charIndex in line.indices) {
+                    yield(gen(index, charIndex, room))
+                }
             }
         }
 
         return s.iterator()
     }
 
+    override fun equals(other: Any?): Boolean {
+        if (other == null) return false
+        if (other !is Room) return false
+        val room = other as Room
 
-    override fun can(prev: Device): Boolean {
-        return prev.output() + 3 >= rating && prev.output() <= rating
+        for(row in rows.indices) {
+            for(column in rows[row].indices) {
+                val my = rows[row][column]
+                val yours = room.rows[row][column]
+                if (my != yours) return false;
+            }
+        }
+
+        return true
     }
 }
 
-class Outlet : Device {
-    var children: List<Device> = listOf()
-    override fun addPossible(def: List<Device>) {
-        children = def
-    }
-
-    override fun total(): Long {
-        if (children.size == 0) return 1;
-        return children.map { it.total() }.sum()
-    }
-}
 
 fun main(args: Array<String>) {
     val file = File("input.txt").readLines()
 
-    val adapters = file.map { it.toLong() }.map { Adapter(it) as Device }
-    val left = adapters.toMutableList();
+    fun test(generator: (Int, Int, Room) -> Seat) {
+        var room = Room(file, generator)
+        var last = room;
+        var counter = 1
+        room = room.ocupy();
 
-    // find hihest
-    val highest = left.sorted().last()
 
-    val computer = Adapter(highest.output() + 3)
+     //   room.print()
 
-    // add adapter in my device
-    left.add(computer)
+        while (!last.equals(room)) {
+            last = room;
+            room = room.ocupy();
 
-    var current : Device = Outlet();
+           // room.print()
 
-    while(!left.isEmpty()) {
-        val next = left.filter { it.can(current) }.sorted().first();
-        left.remove(next)
-        next.setInput(current);
-        current = next;
+            counter++
+        }
+
+
+
+        println("Iterations ${counter} occupied seets ${room.ocupied()}")
     }
 
-    var map = mutableMapOf<Long, Int>()
+    // first
+    test { x, y, r -> Seat(x, y, r) }
+    // second
+    test { x, y, r -> Seat2(x, y, r) }
 
-    for(a in current) {
-        val diff = a.diffrence()
-        if (!map.containsKey(diff)) map.set(diff, 0)
-        map[diff] = map[diff]!! + 1;
-    }
-
-    val onses = map[1]!!
-    val threes = map[3]!!
-
-    println("Fist answer ${onses * threes}")
-
-    // now built tree
-
-    val zero = Outlet()
-    val all = mutableListOf<Device>()
-    all.add(zero)
-    all.addAll(adapters)
-    all.add(computer)
-
-    for(d in all) {
-        val possible = all.filter { it.can(d) && it != d }
-        d.addPossible(possible)
-    }
-
-    val size = zero.total()
-
-    println("Second is ${size}")
 }
